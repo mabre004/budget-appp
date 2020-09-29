7e54f7c77b842ab838871bbf3acb0cf1db955e23
Michael Abreha <michaelabreha@Michaels-MacBook-Pro.local>
2020-09-29 13:33:36 -0400

    Created event listeners and overall design of the project.

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
+// BUDGET CONTROLLER
 var budgetController = (function(){
 
-    var x = 23;
 
-    var add = function(a){
-        return x + a;
-    }
-
-
-    return {
-        publicTest: function(b){
-            return add(b);
-        }
-    }
 })();
 
 
+// UI CONTROLLER
 var UIController = (function(){
 
 
@@ -22,16 +13,35 @@ var UIController = (function(){
 })();
 
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function(){
 
-    return{
-        anotherPublic: function(){
-            console.log(z);
-        }
+        // 1. Get the field input data
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add item to the UI
+
+        // 4. Calculate budget
+
+        // 5. Display the budget on the UI
+
+        console.log("Check if the pressing and hiting \'return' works");
     }
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode === 13 || event.which === 13){
+             ctrlAddItem();
+        }
+    })
 
 })(budgetController, UIController);
+
+
+
+
 
 
