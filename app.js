34b1609d1a3aa9bf08829230e9a229addcf48ba6
Michael Abreha <michaelabreha@Michaels-MacBook-Pro.local>
2020-09-29 22:29:38 -0400

    Set up event listeners.

--- app.js
+++ app.js
@@ -8,7 +8,25 @@ var budgetController = (function(){
 // UI CONTROLLER
 var UIController = (function(){
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
 
+    return {
+        getInput: function(){
+            return{
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+       getDOMstrings: function(){
+           return DOMstrings;
+       }
+    }
 
 })();
 
@@ -16,10 +34,24 @@ var UIController = (function(){
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
+    var DOM = UICtrl.getDOMstrings();
+
+    var setupEventListeners = function(){
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+         document.addEventListener('keypress', function(event){
+        if(event.keyCode === 13 || event.which === 13){
+             ctrlAddItem();
+        }
+
+    });
+
+    }
+
     var ctrlAddItem = function(){
 
         // 1. Get the field input data
-
+        var input = UICtrl.getInput();
         // 2. Add the item to the budget controller
 
         // 3. Add item to the UI
@@ -28,17 +60,44 @@ var controller = (function(budgetCtrl, UICtrl){
 
         // 5. Display the budget on the UI
 
-        console.log("Check if the pressing and hiting \'return' works");
     }
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
-    document.addEventListener('keypress', function(event){
-        if(event.keyCode === 13 || event.which === 13){
-             ctrlAddItem();
+    return {
+        init: function(){
+            console.log('Application has started.');
+            setupEventListeners();
         }
-    })
+    }
 
 })(budgetController, UIController);
+
+controller.init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
