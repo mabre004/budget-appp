eeaed48f5cf9af339a459d031461544d9d4d6940
Michael Abreha <michaelabreha@Michaels-MacBook-Pro.local>
2020-10-03 20:56:00 -0400

    Updated the budget Controller by adding some functionalites when taking input from user.

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ var UIController = (function(){
             return{
                 type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
 
@@ -140,19 +140,32 @@ var controller = (function(budgetCtrl, UICtrl){
 
     }
 
+    var updateBudget = function(){
+
+        // 1. Calculate budget
+
+        // 2. Display the budget on the UI
+
+
+        // 3. Display the budget on the UI
+
+    };
+
     var ctrlAddItem = function(){
         var input, newItem;
        // 1. Get the field input data
         input = UICtrl.getInput();
+
+    if (input.description !== "" && !isNaN(input.value) && input.value > 0){
         // 2. Add the item to the budget controller
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add item to the UI
         UICtrl.addListItem(newItem, input.type);
-        // 4. Calculate budget
+        // 4. Clear the fields
         UICtrl.clearFields();
-        // 5. Clear the fields
+        // 5. Calculate and update budget
+        }
 
-        // 6. Display the budget on the UI
 
     }
 
